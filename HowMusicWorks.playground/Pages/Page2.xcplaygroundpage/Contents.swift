a001e3bf794b9441640b6376b3f12858b396fb72
dantoso <xeddantasvfd@gmail.com>
2022-03-30 14:58:38 -0300

    add: page 3

--- HowMusicWorks.playground/Pages/Page2.xcplaygroundpage/Contents.swift
+++ HowMusicWorks.playground/Pages/Page2.xcplaygroundpage/Contents.swift
@@ -6,8 +6,17 @@
  They are added together to form the blue wave in the bottom.
  Like before, the blue wave is a button that represents the sound you can listen to (by tapping the wave).
  
+ - Note:
+ You can make a white wave not vibrate by making it vibrate in 0 Hz, this way you can observe how one or two waves alone make you feel, before you add the second or third.
+ 
  - Experiment:
- Try to observe how different combination of frequencies makes you feel different things. You can make a white wave not vibrate by making it vibrate in 0 Hz, this way you can observe how two waves alone make you feel, before you add the third.
+ Try to observe how different combination of frequencies makes you feel different things.
+ 
+ In the next page I'll show you how Pythagoras figured tuning out. He observed that frequencies that related to each other in certain ratios make us feel a variation of comfort to discomfort.\
+ Try to see if you can find some nice sounding ratios!
+ 
+ - Experiment:
+ A little hint: try to play a frequency and 2 times that frequency and go from there.
  
  
  ***
