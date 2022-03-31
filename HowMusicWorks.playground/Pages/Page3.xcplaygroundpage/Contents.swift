13164dd0f64c0ab2939a68c85eaf9bed0ee79378
dantoso <xeddantasvfd@gmail.com>
2022-03-30 22:57:24 -0300

    text update

--- HowMusicWorks.playground/Pages/Page3.xcplaygroundpage/Contents.swift
+++ HowMusicWorks.playground/Pages/Page3.xcplaygroundpage/Contents.swift
@@ -1,6 +1,9 @@
 /*:
  
+ # Pythagorean tuning
  
+ A long long time ago, Pythagoras (a very smart greek guy) noticed that the pitch of a sound was determined by the size of the thing vibrating to produce the sound.\
+ Along with that, he also noticed that when two things, one twice the size of the other, vibrate together, they make a very pleasing sound.
  
  
  [Previous](@previous) | [Next](@next)
