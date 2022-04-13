0e99ec9c6fdc9899218bb4a3aab2be2f0266d8e5
dantoso <xeddantasvfd@gmail.com>
2022-04-12 21:56:34 -0300

    first screen text

--- HowMusicWorks-v2.swiftpm/Views/Screens/Screen1.swift
+++ HowMusicWorks-v2.swiftpm/Views/Screens/Screen1.swift
@@ -12,6 +12,7 @@ struct Screen1: View {
 					showInfo = true
 				} label: {
 					Image(systemName: "info.circle")
+						.font(Font.system(size: 24))
 				}
 				Spacer()
 			}
@@ -19,7 +20,7 @@ struct Screen1: View {
 			InterfaceP2(sound: $viewModel.sound)
 			
 		}
-		.popover(isPresented: $showInfo) {
+		.sheet(isPresented: $showInfo, onDismiss: nil) {
 			InfoView()
 		}
 		
@@ -29,23 +30,33 @@ struct Screen1: View {
 
 struct InfoView: View {
 	var body: some View {
-		ScrollView(.vertical) {
+		VStack {
 			Text("Sounds and feelings")
 				.font(.headline)
 				.padding()
 			
-			Text("rhhdshgd")
+			Text("Hey there, welcome to my playground! Here, I'll try to show you how sound waves make us feel things!\nIn this screen you can control the frequency of 3 waves, they are added up to form a resultant wave that you can listen to by pressing play. The resultant and the other 3 waves have a visual representation drawn on the screen.")
 				.padding()
+			
+			Text("So, how does music makes us feel? Well, I'll delay that answer for a bit, first I'd like you to try and do some investigation yourself.\nHere you have 3 waves, so try to isolate 1 of them by making the other 2 vibrate in 0 Hz. Then you can try and see how one frequency alone makes you feel as you change it around.")
+				.padding()
+			
+			HStack {
+				Text("Some hints:\n- Try to find ratios of frequencies that sound nice. If you want a start, try listening to a frequency and double that frequency (ratio of 2/1).")
+				Spacer()
+			}
+			.padding()
+			
+			Spacer()
 
 
 		}
 	}
 }
 
-//struct MyPreviewProvider_Previews: PreviewProvider {
-//	static var previews: some View {
-//		Screen1()
-//			.environmentObject(ViewModel())
-//	}
-//}
+struct MyPreviewProvider_Previews: PreviewProvider {
+	static var previews: some View {
+		InfoView()
+	}
+}
 
